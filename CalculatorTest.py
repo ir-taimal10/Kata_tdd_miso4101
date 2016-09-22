9200b584a2cddbd657504f036e67deb8accd4389
gambastian <sebas.gamba02@gmail.com>
2016-09-21 20:39:05 -0500

    Verde: ciclo TDD 4 prueba string con multiples items

--- CalculatorTest.py
+++ CalculatorTest.py
@@ -56,3 +56,6 @@ class TestStringMethods(unittest.TestCase):
 
     def test_buildResponse_average_two_item_string(self):
         self.assertEquals(Calculator().buildResponse(self.TWO_ITEMS)[3], 1.5)
+
+    def test_buildResponse_average_multiple_item_string(self):
+        self.assertEquals(Calculator().buildResponse(self.MULTIPLE_ITEMS)[3], 4.5)
