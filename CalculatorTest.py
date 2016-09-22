9d238bff7147d59c2040c5046c20d8065467007f
gambastian <sebas.gamba02@gmail.com>
2016-09-21 19:46:47 -0500

    Rojo: ciclo TDD 1 prueba string vacio

--- CalculatorTest.py
+++ CalculatorTest.py
@@ -4,6 +4,8 @@ from Calculator import Calculator
 
 
 class TestStringMethods(unittest.TestCase):
+    def test_calculatorgi(self):
+        self.assert_(Calculator())
 
-   def test_calculatorgi(self):
-       self.assert_(Calculator())
+    def test_buildResponse_empty_string(self):
+        self.assertEquals(Calculator().buildResponse("")[0], 0)
