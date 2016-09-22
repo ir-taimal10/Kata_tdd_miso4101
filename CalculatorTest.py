1d572103d814ec6d3784565a49defceabae854f4
gambastian <sebas.gamba02@gmail.com>
2016-09-21 19:54:35 -0500

    Rojo:  ciclo TDD 1 prueba string con 1 numero

--- CalculatorTest.py
+++ CalculatorTest.py
@@ -9,3 +9,6 @@ class TestStringMethods(unittest.TestCase):
 
     def test_buildResponse_empty_string(self):
         self.assertEquals(Calculator().buildResponse("")[0], 0)
+
+    def test_buildResponse_single_item_string(self):
+        self.assertEquals(Calculator().buildResponse("1")[0], 1)
