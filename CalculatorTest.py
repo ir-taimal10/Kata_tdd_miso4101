eb065bc896aae4b1fc50b08b207c521c7d0d41da
gambastian <sebas.gamba02@gmail.com>
2016-09-21 20:44:29 -0500

    Azul: refactor ciclo TDD Final - Una sola instancia para las pruebas

--- CalculatorTest.py
+++ CalculatorTest.py
@@ -9,56 +9,58 @@ class TestStringMethods(unittest.TestCase):
     TWO_ITEMS = "1,2"
     MULTIPLE_ITEMS = "1,2,3,4,5,6,7,8,9,0"
 
+    calculator = Calculator()
+
     def test_calculatorgi(self):
-        self.assert_(Calculator())
+        self.assert_(self.calculator)
 
     def test_buildResponse_size_empty_string(self):
-        self.assertEquals(Calculator().buildResponse(self.EMPTY)[0], 0)
+        self.assertEquals(self.calculator.buildResponse(self.EMPTY)[0], 0)
 
     def test_buildResponse_size_single_item_string(self):
-        self.assertEquals(Calculator().buildResponse(self.SINGLE_ITEM)[0], 1)
+        self.assertEquals(self.calculator.buildResponse(self.SINGLE_ITEM)[0], 1)
 
     def test_buildResponse_size_two_item_string(self):
-        self.assertEquals(Calculator().buildResponse(self.TWO_ITEMS)[0], 2)
+        self.assertEquals(self.calculator.buildResponse(self.TWO_ITEMS)[0], 2)
 
     def test_buildResponse_size_multiple_item_string(self):
-        self.assertEquals(Calculator().buildResponse(self.MULTIPLE_ITEMS)[0], 10)
+        self.assertEquals(self.calculator.buildResponse(self.MULTIPLE_ITEMS)[0], 10)
 
     def test_buildResponse_minimum_empty_string(self):
-        self.assertEquals(Calculator().buildResponse(self.EMPTY)[1], None)
+        self.assertEquals(self.calculator.buildResponse(self.EMPTY)[1], None)
 
     def test_buildResponse_minimum_single_item_string(self):
-        self.assertEquals(Calculator().buildResponse(self.SINGLE_ITEM)[1], 5)
+        self.assertEquals(self.calculator.buildResponse(self.SINGLE_ITEM)[1], 5)
 
     def test_buildResponse_minimum_two_item_string(self):
-        self.assertEquals(Calculator().buildResponse(self.TWO_ITEMS)[1], 1)
+        self.assertEquals(self.calculator.buildResponse(self.TWO_ITEMS)[1], 1)
 
     def test_buildResponse_minimum_multiple_item_string(self):
-        self.assertEquals(Calculator().buildResponse(self.MULTIPLE_ITEMS)[1], 0)
+        self.assertEquals(self.calculator.buildResponse(self.MULTIPLE_ITEMS)[1], 0)
 
     def test_buildResponse_maximum_empty_string(self):
-        self.assertEquals(Calculator().buildResponse(self.EMPTY)[2], None)
+        self.assertEquals(self.calculator.buildResponse(self.EMPTY)[2], None)
 
     def test_buildResponse_maximum_single_item_string(self):
-        self.assertEquals(Calculator().buildResponse(self.SINGLE_ITEM)[2], 5)
+        self.assertEquals(self.calculator.buildResponse(self.SINGLE_ITEM)[2], 5)
 
     def test_buildResponse_maximum_two_item_string(self):
-        self.assertEquals(Calculator().buildResponse(self.TWO_ITEMS)[2], 2)
+        self.assertEquals(self.calculator.buildResponse(self.TWO_ITEMS)[2], 2)
 
     def test_buildResponse_maximum_multiple_item_string(self):
-        self.assertEquals(Calculator().buildResponse(self.MULTIPLE_ITEMS)[2], 9)
+        self.assertEquals(self.calculator.buildResponse(self.MULTIPLE_ITEMS)[2], 9)
 
     def test_buildResponse_average_empty_string(self):
-        self.assertEquals(Calculator().buildResponse(self.EMPTY)[3], None)
+        self.assertEquals(self.calculator.buildResponse(self.EMPTY)[3], None)
 
     def test_buildResponse_average_single_item_string(self):
-        self.assertEquals(Calculator().buildResponse(self.SINGLE_ITEM)[3], 5)
+        self.assertEquals(self.calculator.buildResponse(self.SINGLE_ITEM)[3], 5)
 
     def test_buildResponse_average_two_item_string(self):
-        self.assertEquals(Calculator().buildResponse(self.TWO_ITEMS)[3], 1.5)
+        self.assertEquals(self.calculator.buildResponse(self.TWO_ITEMS)[3], 1.5)
 
     def test_buildResponse_average_multiple_item_string(self):
-        self.assertEquals(Calculator().buildResponse(self.MULTIPLE_ITEMS)[3], 4.5)
+        self.assertEquals(self.calculator.buildResponse(self.MULTIPLE_ITEMS)[3], 4.5)
 
     def test_buildResponse_full(self):
-        self.assertEquals(Calculator().buildResponse(self.MULTIPLE_ITEMS), [10,0,9,4.5])
+        self.assertEquals(self.calculator.buildResponse(self.MULTIPLE_ITEMS), [10, 0, 9, 4.5])
