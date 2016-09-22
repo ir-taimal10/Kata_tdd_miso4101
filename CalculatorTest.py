4182539fa3b8c1936f2d5e40026f75430ba13ece
gambastian <sebas.gamba02@gmail.com>
2016-09-21 20:00:47 -0500

    Verde:  ciclo TDD 1 prueba string con multiples numeros

--- CalculatorTest.py
+++ CalculatorTest.py
@@ -15,3 +15,6 @@ class TestStringMethods(unittest.TestCase):
 
     def test_buildResponse_two_item_string(self):
         self.assertEquals(Calculator().buildResponse("1,2")[0], 2)
+
+    def test_buildResponse_multiple_item_string(self):
+        self.assertEquals(Calculator().buildResponse("1,2,3,4,5,6,7,8,9,0")[0], 10)
